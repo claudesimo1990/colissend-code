14df6adb27512a8ed193b796a48d4fafa6ee2b8e
claudesimo1990 <45970298+claudesimo1990@users.noreply.github.com>
2022-03-26 11:50:00 +0100

    Update header

--- templates/mail/layout/html/header.blade.php
+++ templates/mail/layout/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) !== 'Laravel')
-<img src="{{ asset('images/colissend/logo.png') }}" class="logo" alt="colissend Logo">
+    <img src="{{ asset('images/colissend/logo.png') }}" width="150px" alt="colissend Logo">
 @else
 {{ $slot }}
 @endif
