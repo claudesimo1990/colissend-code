b1c234e5ccd9f01a92145afea079642300d4d0df
claudesimo1990 <45970298+claudesimo1990@users.noreply.github.com>
2022-05-02 15:41:34 +0200

    wip

---
+++ public/js/adminController.js
@@ -0,0 +1,19 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*************************************************!*\
+  !*** ./assets/js/controller/adminController.ts ***!
+  \*************************************************/
+
+/*import Vue from 'vue';
+
+import statsComponent from '../components/admin/StatsComponent.vue'
+
+
+const app = new Vue({
+    components: {
+        statsComponent
+    }
+}).$mount('#admin');*/
+/******/ })()
+;
