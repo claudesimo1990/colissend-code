bd16d102cc6b65165434da8128723d99900bf9b0
claudesimo1990 <45970298+claudesimo1990@users.noreply.github.com>
2022-06-30 21:48:24 +0200

    wip

--- templates/app/user/profile/bank.blade.php
+++ templates/app/user/profile/bank.blade.php
@@ -11,42 +11,42 @@
             <div class="row">
                 <div class="col-lg-6">
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="adresse" placeholder="adresse" name="adresse">
-                        <label for="adresse">Adresse</label>
+                        <input type="text" class="form-control @error('bank_address_1') is-invalid @enderror" id="bank_address_1" placeholder="adresse" name="bank_address_1" value="{{ $profile->bank_address_1 }}">
+                        <label for="bank_adresse_1">Adresse</label>
                     </div>
                 </div>
                 <div class="col-lg-6">
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="adresse2" placeholder="" name="adresse2">
-                        <label for="adresse2">Complément d'adresse</label>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="postal-code" placeholder="" name="postal-code">
-                        <label for="postal-code">Code postal</label>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="country" placeholder="" name="country">
-                        <label for="country">Ville</label>
+                        <input type="text" class="form-control" id="bank_address_2" placeholder="" name="bank_address_2" value="{{ $profile->bank_address_2 }}">
+                        <label for="bank_address_2">Complément d'adresse</label>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-6">
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="bank-owner" placeholder="" name="bank-owner">
-                        <label for="bank-owner">Titulaire du compte</label>
+                        <input type="text" class="form-control @error('bank_postal_code') is-invalid @enderror" id="bank_postal_code" placeholder="" name="bank_postal_code" value="{{ $profile->bank_postal_code }}">
+                        <label for="bank_postal_code">Code postal</label>
                     </div>
                 </div>
                 <div class="col-lg-6">
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="iban" placeholder="" name="iban">
-                        <label for="iban">IBAN</label>
+                        <input type="text" class="form-control  @error('bank_city') is-invalid @enderror" id="bank_city" placeholder="" name="bank_city" value="{{ $profile->bank_city }}">
+                        <label for="bank_city">Ville</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control  @error('bank_owner') is-invalid @enderror" id="bank_owner" placeholder="" name="bank_owner" value="{{ $profile->bank_owner }}">
+                        <label for="bank_owner">Titulaire du compte</label>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control @error('bank_iban') is-invalid @enderror" id="bank_iban" placeholder="" name="bank_iban" value="{{ $profile->bank_iban }}">
+                        <label for="bank_iban">IBAN</label>
                     </div>
                 </div>
             </div>
