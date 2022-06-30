0988179cc547b96059ac0428660b92f8ca2ac824
claudesimo1990 <45970298+claudesimo1990@users.noreply.github.com>
2022-06-30 21:54:23 +0200

    wip

---
+++ app/Console/Commands/ImportCountriesFromJson.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Country;
+use Illuminate\Console\Command;
+
+class ImportCountriesFromJson extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'Countries:import';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import Countries List from json';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Country::truncate();
+
+        $json = storage_path('country.json');
+        $json = json_decode(file_get_contents($json), true);
+
+        foreach ($json as $country) {
+            Country::create($country);
+        }
+
+        return Command::SUCCESS;
+    }
+}
---
+++ app/Models/Identity.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Identity extends Model implements HasMedia
+{
+    use HasFactory;
+    use InteractsWithMedia;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('identities')
+            ->singleFile();
+    }
+
+    public function registerAllMediaConversions(): void
+    {
+        $this->addMediaConversion('thumb')
+            ->width(1024)
+            ->height(768)
+            ->sharpen(10);
+    }
+}
