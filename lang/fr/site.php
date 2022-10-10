51330288dd2257d436977fe2b279c9cd86622ebe
Jean Claude Simo <jeanclaude.simo@abus-kransysteme.de>
2022-10-10 08:19:57 +0200

    some changes

---
+++ lang/fr/site.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'nav' => [
+        'home' => 'Accueil',
+        'posts' => 'Voir les annonces',
+        'travel' => 'Poster un voyage',
+        'packs' => 'Expedier un colis',
+        'shop' => 'Boutique',
+        'inscription' => 'S\'inscrire',
+        'connexion' => 'Se connecter',
+    ],
+
+    'header' => [
+        'with_colissend' => 'Avec Colissend',
+        'body_why_use_colissend' => 'Faciliter vous la vente de vos kilos Et l\'envoi de vos Colis en toute securité vers multiples destinations.',
+    ],
+
+    'how_use_colissend' => 'Pourquoi choisir Colissend ?',
+
+];
